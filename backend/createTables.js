7b55b11744c075f797c38c4f23d9aa2ecc4d4fc8
Vani-KK <vani.ds22@sahyadri.edu.in>
2024-12-27 23:41:59 +0530

    data size changed for tables

--- backend/createTables.js
+++ backend/createTables.js
@@ -4,41 +4,41 @@ const pool = require("./db");
 const createTables = async () => {
   const queries = [
     `CREATE TABLE IF NOT EXISTS lawyer (
-            lid VARCHAR(10),
-            lname VARCHAR(15),
-            ltype VARCHAR(15),
+            lid VARCHAR(20),
+            lname VARCHAR(100),
+            ltype VARCHAR(100),
             lphone BIGINT,
-            laddress VARCHAR(20),
+            laddress VARCHAR(100),
             PRIMARY KEY(lid)
         );`,
 
     `CREATE TABLE IF NOT EXISTS client (
-            cid VARCHAR(10),
-            lid VARCHAR(10),
-            cname VARCHAR(15),
+            cid VARCHAR(20),
+            lid VARCHAR(20),
+            cname VARCHAR(100),
             cphone BIGINT,
-            caddress VARCHAR(20),
+            caddress VARCHAR(100),
             PRIMARY KEY (cid),
             FOREIGN KEY (lid) REFERENCES lawyer (lid) ON DELETE CASCADE
         );`,
 
     `CREATE TABLE IF NOT EXISTS court (
-            court_id VARCHAR(10),
-            court_location VARCHAR(15),
-            judge_name VARCHAR(15),
+            court_id VARCHAR(20),
+            court_location VARCHAR(100),
+            judge_name VARCHAR(100),
             PRIMARY KEY (court_id)
         );`,
 
     `CREATE TABLE IF NOT EXISTS cases (
-            case_id VARCHAR(10),
-            case_name VARCHAR(20),
-            case_type VARCHAR(20),
-            case_status VARCHAR(20),
+            case_id VARCHAR(20),
+            case_name VARCHAR(100),
+            case_type VARCHAR(100),
+            case_status VARCHAR(100),
             case_strdate DATE,
             case_enddate DATE,
-            cid VARCHAR(10),
-            lid VARCHAR(10),
-            court_id VARCHAR(10),
+            cid VARCHAR(20),
+            lid VARCHAR(20),
+            court_id VARCHAR(20),
             PRIMARY KEY (case_id),
             FOREIGN KEY (cid) REFERENCES client (cid) ON DELETE CASCADE,
             FOREIGN KEY (lid) REFERENCES lawyer (lid) ON DELETE CASCADE,
@@ -46,14 +46,14 @@ const createTables = async () => {
         );`,
 
     `CREATE TABLE IF NOT EXISTS document (
-            d_id VARCHAR(10),
-            pwitness VARCHAR(20),
-            opposition VARCHAR(20),
-            defence VARCHAR(20),
-            legal_doc VARCHAR(20),
-            refernce_doc VARCHAR(20),
-            case_id VARCHAR(10),
-            lid VARCHAR(10),
+            d_id VARCHAR(20),
+            pwitness VARCHAR(100),
+            opposition VARCHAR(100),
+            defence VARCHAR(100),
+            legal_doc VARCHAR(100),
+            refernce_doc VARCHAR(100),
+            case_id VARCHAR(20),
+            lid VARCHAR(20),
             PRIMARY KEY(d_id),
             FOREIGN KEY (case_id) REFERENCES cases (case_id) ON DELETE CASCADE,
             FOREIGN KEY (lid) REFERENCES lawyer (lid) ON DELETE CASCADE
